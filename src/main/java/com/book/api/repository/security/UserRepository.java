b63c854d932037c8401052e803476344122f9b94
yelo-o <flyordig@gmail.com>
2023-11-24 00:53:17 +0900

    add repository interface

---
+++ src/main/java/com/book/api/repository/security/UserRepository.java
@@ -0,0 +1,11 @@
+package com.book.api.repository.security;
+
+import com.book.api.models.security.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
