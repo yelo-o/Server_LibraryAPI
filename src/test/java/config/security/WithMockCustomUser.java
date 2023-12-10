7603a4cbd485788d6bbcc2d1f066e5dbf653546e
yelo-o <flyordig@gmail.com>
2023-12-10 14:22:02 +0900

    커스텀 MockUser 인터페이스 : 추후에 테스트 예정

---
+++ src/test/java/config/security/WithMockCustomUser.java
@@ -0,0 +1,13 @@
+package config.security;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+    String userName() default "user";
+    String role() default "USER";
+}
